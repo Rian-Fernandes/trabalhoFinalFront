534cc0c308b5ae54a6e392b4b53002e4ef7d1557
Felipe Sutter <lipeps61@gmail.com>
2023-09-28 14:36:30 -0300

    Correção do dark mode

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -3,24 +3,26 @@ const themeButton = document.querySelector("#theme-checkbox");
 
 themeButton.addEventListener("change", function () {
   const isDarkMode = themeButton.checked;
-  
+
   html.classList.toggle("dark-mode", isDarkMode);
-  
+
   const imgHeader = document.getElementById("logo");
   imgHeader.src = isDarkMode ? "./images/logoDark.gif" : "./images/logo.gif";
-  
+
   const imgFooter = document.getElementById("logoF");
   imgFooter.src = isDarkMode ? "./images/logoDark.gif" : "./images/logo.gif";
+
+  localStorage.setItem("dark-mode", isDarkMode);
 });
 
 document.addEventListener("DOMContentLoaded", function () {
   const isDarkMode = localStorage.getItem("dark-mode") === "true";
   html.classList.toggle("dark-mode", isDarkMode);
   themeButton.checked = isDarkMode;
-  
+
   const imgHeader = document.getElementById("logo");
   imgHeader.src = isDarkMode ? "./images/logoDark.gif" : "./images/logo.gif";
-  
+
   const imgFooter = document.getElementById("logoF");
   imgFooter.src = isDarkMode ? "./images/logoDark.gif" : "./images/logo.gif";
 });
