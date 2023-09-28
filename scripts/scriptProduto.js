9e47a9e38cb7bde86d310d0746769ececeb62345
Felipe Sutter <lipeps61@gmail.com>
2023-09-27 23:13:56 -0300

    adição da API na página de descrição

--- scripts/scriptProduto.js
+++ scripts/scriptProduto.js
@@ -1,20 +1,20 @@
-async function getProduto() {
-  const url = "https://651378698e505cebc2e9e3dc.mockapi.io/produtos";
+(async function getProduto() {
+  let params = new URL(document.location).searchParams;
+  let id = params.get("id");
+  const url = "https://651378698e505cebc2e9e3dc.mockapi.io/produtos/" + id;
   const response = await fetch(url);
   const produto = await response.json();
-  console.log(produto);
-  return produto[0];
-}
+  mostrarProduto(produto);
+})();
 
-async function mostrarProduto() {
-  const prod = await getProduto();
-  console.log(prod);
-  // const container = document.querySelector("meu-container");
-  // //const img = document.createElement("#product-img");
-  // const img = document.createElement("img");
-  // const title = document.getElementById("product-title");
-  // const description = document.getElementById("product-description");
-  // const price = document.getElementById("product-price");
-  // img.src = prod.url;
-  // container.appendChild(img);
+async function mostrarProduto(prod) {
+  const container = document.querySelector("meu-container");
+  const img = document.getElementById("product-img");
+  const title = document.getElementById("product-title");
+  const description = document.getElementById("product-description");
+  const price = document.getElementById("product-price");
+  img.src = prod.image;
+  title.innerText = prod.title;
+  description.innerText = prod.description;
+  price.innerText = prod.price;
 }
