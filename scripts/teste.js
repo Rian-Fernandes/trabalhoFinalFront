eecf81973887ec1df66fd259a2c646bfbcb3717c
Rian-Fernandes <rian10fernandes@hotmail.com>
2023-09-28 21:59:58 -0300

    Finalmente o trabalho completo e acabadogit add .!

--- scripts/teste.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("message", function (event) {
-  const productInfo = JSON.parse(event.data);
-
-  document.getElementById("product-img").src = productInfo.image;
-  document.getElementById("product-title").textContent = productInfo.title;
-  document.getElementById("product-description").textContent =
-    productInfo.description;
-  document.getElementById("product-price").textContent = productInfo.price;
-});
